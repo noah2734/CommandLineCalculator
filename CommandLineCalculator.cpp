6c1d6ae7ad6898077255e05b0e8ed7f1add5f385
Noah Robert Mcclelland <noahmcclelland1738@gmail.com>
2023-06-18 20:05:25 -0700

    More input validation fixes

--- CommandLineCalculator.cpp
+++ CommandLineCalculator.cpp
@@ -2,35 +2,65 @@
 #include <sstream>
 #include <limits>
 
-int calculate(std::string equation);
+double calculate(std::string equation);
+std::string combineTerms(int a, int b, char sign);
+bool isWhitespace(unsigned char c);
+bool validateEquation(std::string equation);
 
 int main() {
-    char exit = 'n';
     std::string equation;
     double result;
-
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
+    char Continue; 
+    bool isValid = false;
     do {
-        std::cout << "Enter an equation using (+, -, *, /) then press enter\nEquation: ";
-        getline(std::cin, equation);
+        do {
+            std::cout << "Enter an equation using (+, -, *, /) then press enter\n\nEquation: ";
+            getline(std::cin, equation);
+            equation.erase(std::remove_if(equation.begin(), equation.end(), isWhitespace), equation.end());
+            if(validateEquation(equation)) {
+                isValid = true;
+            } else {
+                isValid = false;
+            }
+        } while(!isValid);
         result = calculate(equation);
 
-        std::cout << "\n" << equation << " = " << result;
-        std::cout << "Continue?('y' or 'n'): ";
-        std::cin >> exit;
+        std::cout << "\n" << equation << " = " << result << "\n";
+        do {
+            std::cout << "\nContinue?('y' or 'n'): ";
+            std::cin >> Continue;
+            if (Continue == 'y' || Continue == 'n') {
+                break;
+            } else {
+                std::cout << "\nInvalid input\n";
+                Continue = 'z';
+            }
+        } while (Continue == 'z');
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        if (exit != 'y' && exit != 'n') {
-            do {
-                std::cout << "Invalid input, please enter 'y' or 'n'. ";
-                std::cout << "Continue?('y' or 'n'): ";
-                std::cin >> exit;
-            } while (exit != 'y' || exit != 'n');
-        }
-        
-    } while (exit == 'n');
+    } while (Continue == 'y');
 
     return 0;
 }
 
+double calculate(std::string equation) {
+    char current;
+    double total = 0;
+    int length = equation.size();
+    for (int i = 0; i < length; i++) {
+        current = equation[i];
+    }
+    return total;
+}
+
+bool isWhitespace(unsigned char c) {
+    if (c == ' ' || c == '\t' || c == '\n' ||
+        c == '\r' || c == '\f' || c == '\v') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool validateEquation(std::string equation) {
+    return true;
+}
