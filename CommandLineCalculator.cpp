9a8108f468ede76f5ab16a42d834d410db818a9f
Noah Robert Mcclelland <noahmcclelland1738@gmail.com>
2023-06-18 17:24:35 -0700

    Input validation to exit condition

--- CommandLineCalculator.cpp
+++ CommandLineCalculator.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
 #include <sstream>
+#include <limits>
 
 int calculate(std::string equation);
 
 int main() {
-    bool exit = false;
+    char exit = 'n';
     std::string equation;
     double result;
+
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
     do {
-        std::cout << "Enter an equation using (+, -, *, /) then press enter";
+        std::cout << "Enter an equation using (+, -, *, /) then press enter\nEquation: ";
         getline(std::cin, equation);
         result = calculate(equation);
+
         std::cout << "\n" << equation << " = " << result;
-    } while (!exit);
+        std::cout << "Continue?('y' or 'n'): ";
+        std::cin >> exit;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+        if (exit != 'y' && exit != 'n') {
+            do {
+                std::cout << "Invalid input, please enter 'y' or 'n'. ";
+                std::cout << "Continue?('y' or 'n'): ";
+                std::cin >> exit;
+            } while (exit != 'y' || exit != 'n');
+        }
+        
+    } while (exit == 'n');
+
     return 0;
 }
 
