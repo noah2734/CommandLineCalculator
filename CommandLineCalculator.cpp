05d73c1bd6fcf6d5d02ce9b53ac85476f9098fbe
Noah Robert Mcclelland <noahmcclelland1738@gmail.com>
2023-06-20 10:12:05 -0700

    Implemented evaluation using RPN

--- CommandLineCalculator.cpp
+++ CommandLineCalculator.cpp
@@ -1,161 +1,400 @@
+//  CommandLineCalculator uses the following materials.
+//  (1) Wikipedia article [Shunting-yard algorithm](https://en.wikipedia.org/wiki/Shunting-yard_algorithm), 
+//  which is released under the [Creative Commons Attribution-Share-Alike License 3.0](https://creativecommons.org/licenses/by-sa/3.0/).
+//  (2) [Implementation notes for unary operators in Shunting-Yard algorithm](https://stackoverflow.com/a/5240912) by Austin Taylor
+//  which is released under the [Creative Commons Attribution-Share-Alike License 2.5](https://creativecommons.org/licenses/by-sa/2.5/).
 #include <iostream>
 #include <sstream>
-#include <limits>
 #include <string>
+#include <vector>
+#include <deque>
+#include <tuple>
+#include <stdio.h>
+#include <math.h>
 
-double calculate(std::string equation);
-std::string combineTerms(int a, int b, char sign);
-bool isWhitespace(unsigned char c);
-bool validateEquation(std::string equation);
+static const char* reportFmt = "|%-5s|%-32s|%17s| %s\n";
+
+class Token {
+public:
+    enum class Type {
+        Unknown,
+        Number,
+        Operator,
+        LeftParen,
+        RightParen
+    };
+
+    Token(Type type,
+          const std::string& s,
+          int precedence = -1,
+          bool rightAssociative = false,
+          bool unary = false
+    )
+        : type { type }
+        , str ( s )
+        , precedence { precedence }
+        , rightAssociative { rightAssociative }
+        , unary { unary }
+    {}
+
+    const Type type;
+    const std::string str;
+    const int precedence;
+    const bool rightAssociative;
+    const bool unary;
+};
+
+//Overload the stream insertoion operator
+std::ostream& operator<<(std::ostream& os, const Token& token) {
+    os << token.str;
+    return os;
+}
+
+// Debug output
+template<class T, class U>
+void debugReport (
+    const Token& token,
+    const T& queue,
+    const U& stack,
+    const std::string& comment = ""
+) {
+    std::ostringstream ossQueue;
+    for (const auto& t : queue) {
+        ossQueue << " " << t;
+    }
+
+    std::ostringstream ossStack;
+    for (const auto& t : stack) {
+        ossStack << " " << t;
+    }
+
+    printf(reportFmt,
+           token.str.c_str(),
+           ossQueue.str().c_str(),
+           ossStack.str().c_str(),
+           comment.c_str()
+    );
+}
+
+
+std::deque<Token> exprToTokens(const std::string& expr) {
+    std::deque<Token> tokens;
+
+    for (const auto* p = expr.c_str(); *p; ++p) {
+        if (isblank(*p)) {
+            // do nothing if character is a space
+        } else if (isdigit(*p)) {
+            const auto* b = p; // set be to the start of the number
+            while (isdigit(*p)) {
+                ++p; // increment p untill we reach the end of the number
+            }
+            const auto s = std::string(b, p);
+            tokens.push_back(Token { Token::Type::Number, s });
+            --p;
+        } else {
+            Token::Type t = Token::Type::Unknown;
+            int precedence = -1;
+            bool rightAssociative = false;
+            bool unary = false;
+            char c = *p;
+            switch(c) {
+                case '(':
+                    t = Token::Type::LeftParen;
+                    break;
+
+                case ')':
+                    t = Token::Type::RightParen;
+                    break;
+
+                case '^':
+                    t = Token::Type::Operator;
+                    precedence = 4;
+                    rightAssociative = true;
+                    break;
+
+                case '*':
+                    t = Token::Type::Operator;
+                    precedence = 3;
+                    break;
+                case '/':
+                    t = Token::Type::Operator;
+                    precedence = 3;
+                    break;
+
+                case '+':
+                    t = Token::Type::Operator;
+                    precedence = 2;
+                    break;
+
+                case '-':
+                    // If current token is '-'
+                    // and if it is the first token, or preceded by another operator, or left-paren,
+                    if(tokens.empty() || 
+                    tokens.back().type == Token::Type::Operator || 
+                    tokens.back().type == Token::Type::LeftParen) 
+                    {
+                        // it's unary '-'
+                        // note#1 : 'm' is a special operator name for unary '-'
+                        // note#2 : It has highest precedence than any of the infix operators
+                        unary = true;
+                        c = 'm';
+                        t = Token::Type::Operator;
+                        precedence = 5;
+                    } else {
+                        // otherwise, it's binary '-'
+                        t = Token::Type::Operator;
+                        precedence = 2;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            const auto s = std::string(1, c);
+            tokens.push_back(Token { t, s, precedence, rightAssociative, unary });
+        }
+    }
+
+    return tokens;
+}
+
+std::deque<Token> shuntingYard(const std::deque<Token>& tokens) {
+    std::deque<Token> queue;
+    std::vector<Token> stack;
+
+    // While there are tokens to be read:
+    for (auto token : tokens) {
+        // Read a token
+        switch (token.type) {
+            case Token::Type::Number:
+                // If the token is a number, then add it to the output queue
+                queue.push_back(token);
+                break;
+
+            case Token::Type::Operator:
+                {
+                    // If the token is operator, o1, then:
+                    const auto o1 = token;
+
+                    // while there is an operator token,
+                    while (!stack.empty()) {
+                        // o2, at the top of the stack, and
+                        const auto o2 = stack.back();
+
+                        // either o1 is left-associative and its precedence is
+                        // *less than or equal* to that of o2,
+                        // or o1 is right-associative, and has precedence
+                        // *less than* that of o2,
+                        if ((!o1.rightAssociative && o1.precedence <= o2.precedence) || (o1.rightAssociative && o1.precedence < o2.precedence)) {
+                            // then pop o2 off the stack,
+                            stack.pop_back();
+                            // onto the output queue
+                            queue.push_back(o2);
+
+                            continue;
+                        }
+
+                        // otherwise, exit.
+                        break;
+                    }
+
+                    // push o1 onto the stack
+                    stack.push_back(o1);
+                }
+                break;
+
+            case Token::Type::LeftParen:
+                // If token is left parenthesis, then push it onto the stack
+                stack.push_back(token);
+                break;
+            
+            case Token::Type::RightParen:
+                // If token is right parenthesis:
+                {
+                    bool match = false;
+
+                    // Until the token at the top of the stack is
+                    // a left parenthesis,
+                    while (!stack.empty() && stack.back().type != Token::Type::LeftParen) {
+                        // pop operators off the stack
+                        // onto the output queue.
+                        queue.push_back(stack.back());
+                        stack.pop_back();
+                        match = true;
+                    }
+
+                    if (!match && stack.empty()) {
+                        // If the stack runs out without finding a left parenthesis,
+                        // then there are mismatched parentheses
+                        printf("RightParen error (%s)\n", token.str.c_str());
+                        return {};
+                    }
+
+                    // Pop the left parenthesis from the stack,
+                    // but not onto the output queue
+                    stack.pop_back();
+                }
+                break;
+            
+            default:
+                printf("error (%s)\n", token.str.c_str());
+                return {};
+        }
+
+        debugReport(token, queue, stack);
+    }
+
+    // When there are no more tokens to read:
+    // then there are still operator tokens in the stack:
+    while (!stack.empty()) {
+        // If the operator token on the top of the stack is a parenthesis
+        // then there are mismatched parenthesis
+        if (stack.empty()) {
+            printf("Mismatched parentheses error\n");
+            return {};
+        }
+
+        // Pop the operator onto the output queue
+        queue.push_back(std::move(stack.back()));
+        stack.pop_back();
+    }
+
+    debugReport(Token { Token::Type::Unknown, "End" }, queue, stack);
+
+    //Exit
+    return queue;       
+}
+
+
+int32_t compute(const std::string& expr) {
+    printf("Tokenize\n)");
+    printf(reportFmt, "Token", "Queue", "Stack", "");
+    const auto tokens = exprToTokens(expr);
+    auto queue = shuntingYard(tokens);
+    std::vector<int> stack;
+
+    printf("\nCalculation\n");
+    printf(reportFmt, "Token", "Queue", "Stack", "");
+
+    while(!queue.empty()) {
+        std::string op;
+
+        const auto token = queue.front();
+        queue.pop_front();
+        switch(token.type) {
+            case Token::Type::Number:
+                stack.push_back(std::stoi(token.str));
+                op = "Push" + token.str;
+                break;
+
+            case Token::Type::Operator:
+                {
+                    if (token.unary) {
+                        //unary operators
+                        const auto rhs = stack.back();
+                        stack.pop_back();
+                        switch(token.str[0]) {
+                            default:
+                                printf("Operator error [%s]\n", token.str.c_str());
+                                exit(0);
+                                break;
+                            case 'm':
+                                stack.push_back(-rhs);
+                                break;
+                        }
+                        op = "Push (unary) " + token.str + " " + std::to_string(rhs);
+                    } else {
+                        // binary operators
+                        const auto rhs = stack.back();
+                        stack.pop_back();
+                        const auto lhs = stack.back();
+                        stack.pop_back();
+
+                        switch(token.str[0]) {
+                            default:
+                                printf("Operator error [%s]\n", token.str.c_str());
+                                exit(0);
+                                break;
+                            case '^':
+                                stack.push_back(static_cast<int>(pow(lhs, rhs)));
+                                break;
+                            case '*':
+                                stack.push_back(lhs * rhs);
+                                break;
+                            case '/':
+                                stack.push_back(lhs / rhs);
+                                break;
+                            case '+':
+                                stack.push_back(lhs + rhs);
+                                break;
+                            case '-':
+                                stack.push_back(lhs - rhs);
+                                break;
+                        }
+                        op = "Push " + std::to_string(lhs) + " " + token.str + " " + std::to_string(rhs);
+                    }
+                }
+                break;
+
+            default:
+                printf("Token error\n");
+                exit(0);
+        }
+        debugReport(token, queue, stack, op);
+    }
+
+    return stack.back();
+}
 
 int main() {
-    std::string equation;
-    double result;
-    char Continue; 
-    bool isValid = false;
-    do {
-        do {
-            std::cout << "Enter an equation using (+, -, *, /) then press enter\n\nEquation: ";
-            getline(std::cin, equation);
-            equation.erase(std::remove_if(equation.begin(), equation.end(), isWhitespace), equation.end());
-            if(validateEquation(equation)) {
-                isValid = true;
-            } else {
-                isValid = false;
-            }
-        } while(!isValid);
-        result = calculate(equation);
-
-        std::cout << "\n" << equation << " = " << result << "\n";
-        do {
-            std::cout << "\nContinue?('y' or 'n'): ";
-            std::cin >> Continue;
-            if (Continue == 'y' || Continue == 'n') {
-                break;
-            } else {
-                std::cout << "\nInvalid input\n";
-                Continue = 'z';
-            }
-        } while (Continue == 'z');
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    } while (Continue == 'y');
-
-    return 0;
+    std::string expr;
+    printf("Enter an equation using (+, -, *, /, ^)\n");
+    printf("Equation: ");
+    getline(std::cin, expr);
+    const int32_t result = compute(expr);
+    printf("\n\n\n----------------------------------------------------");
+    printf("\n%s = %d\n", expr.c_str(), result);
+    printf("----------------------------------------------------");
 }
 
-double calculate(std::string equation) {
-    char current;
-    double total = 0;
-    int length = equation.size();
 
-    int startNum1 = 0;
-    int endNum1 = 0;
-    int startNum2 = 0;
-    int endNum2 = 0;
-    double num1;
-    double num2;
-    int startParenthesis = 0;
-    int endParenthesis = 0;
-    double combined;
 
-    for (int i = 0; i < length; i++) {
-        current = equation[i];
-        //Operations within parentheses need to be evaluated first recursively
-        if (current == '(') {
-            startParenthesis = current;
-            int parenthesisCounter = 0;
-            std::string subEquation;
+/*int main() {
+    printf("Shunting-yard\n");
 
-            for (int j = startParenthesis + 1; j < length; j++) {
-                if (equation[j] == ')' && parenthesisCounter == 0) {
-                    endParenthesis = j;
-                    //recursively call calculate on the sub-equation within the parentheses
-                    subEquation = equation.substr(startParenthesis+1, endParenthesis-1);
-                    combined = calculate(subEquation);
-                    equation = equation.substr(0, startParenthesis+1) + std::to_string(combined) + equation.substr(endParenthesis-1, length-1);                }
+    const std::vector<std::tuple<std::string, int32_t>> testCases = {
+        { "3+4*2/(1-5)^2^3" , 3     },  // Wikipedia's example
+        { "(2*3+3*4)"       , 18    },  // Report from @2kaud.
+        { "(3)+(4)"         , 7     },  // Report from @kayshav.
+        { "(-3)*(-2^3)"     , 24    },  // Unary '-'
+    };
 
-                else if (equation[j] == '(') {
-                    parenthesisCounter++;
-                }
-            }
+    int errorCount = 0;
+    int testCount = 0;
 
-        }
-        //evaluate division and multiplication
-        else if (current == '*' || current == '/') {
-            endNum1 = i - 1;
-            num1 = std::stod(equation.substr(startNum1, endNum1));
-            startNum2 = i + 1;
-            for (int j = startNum2; j < length; j++) {
-                if (equation[j] == '+' || equation[j] == '-' || equation[j] == '*' || equation[j] == '/' || (j + 1) == length) {
-                    endNum2 = j - 1;
-                }
-            }
-            num2 = std::stod(equation.substr(startNum2, endNum2));
-            if (current == '*') {
-                combined = num1*num2;
-            } else {
-                combined = num1/num2;
-            }
-            equation = equation.substr(0, startNum1-1) + std::to_string(combined) + equation.substr(endNum2+1, length-1);
+    for(const auto& testCase : testCases) {
+        testCount += 1;
+        const std::string& expr = std::get<0>(testCase);
+        const int32_t expected = std::get<1>(testCase);
+        printf("expr = %s\n\n", expr.c_str());
+
+        const int32_t result = compute(expr);
+
+        printf("\n  result = %d, expected = %d, ", result, expected);
+        if(result == expected) {
+            printf("OK\n\n\n");
+        } else {
+            printf("NG\n\n\n");
+            errorCount += 1;
         }
     }
-    startNum1 = 0;
-    endNum1 = 0;
-    startNum2 = 0;
-    endNum2 = 0;
-    //add or subtract all liked terms
-    for (int i = 0; i < length; i++) {
-        current = equation[i];
-        if (current == '+' || current == '-') {
-            endNum1 = i - 1;
-            std::cout << equation.substr(0, length);
-            num1 = std::stod(equation.substr(startNum1, endNum1+1));
-            startNum2 = i;
-            for (int j = startNum2; j < length; j++) {
-                if (equation[j] == '+' || equation[j] == '-' || (j + 1) == length) {
-                    endNum2 = j - 1;
-                }
-            }
-            num2 = std::stod(equation.substr(startNum2, endNum2+2));
-            if (current == '+') {
-                combined = num1+num2;
-            } else {
-                combined = num1-num2;
-            }
-            equation.replace(startNum1, endNum2, std::to_string(combined));
-            std::cout << equation << std::endl;
-            i = endNum1;
-            startNum1 = startNum2;
-        }
 
-    }
-    int count = 0;
-    //see if all terms are combined
-    for (int i = 0; i < length; i++) {
-        if(equation[i] == '+' || equation[i] == '-' || equation[i] == '*' || equation[i] == '/' || equation[i] == '(' || equation[i] == ')') {
-            count++;
-        }
-    }
-    //if all terms are combined, return the total
-    if(count == 0) {
-        total = std::stod(equation);
-        return total;
-    } else return -1;
-}
-
-bool isWhitespace(unsigned char c) {
-    if (c == ' ' || c == '\t' || c == '\n' ||
-        c == '\r' || c == '\f' || c == '\v') {
-        return true;
+    if(errorCount != 0 || testCount != static_cast<int>(testCases.size())) {
+        printf("ERROR (errorCount=%d, testCount=%d)\n", errorCount, testCount);
+        exit(EXIT_FAILURE);
     } else {
-        return false;
+        printf("OK\n");
+        exit(EXIT_SUCCESS);
     }
-}
+}*/
 
-bool validateEquation(std::string equation) {
-    int length = equation.size();
-    for (int i = 0; i < length; i++) {
-        //to be implemented eventually
-    }
-    return true;
-}
